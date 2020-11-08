27a9ec15f9fcd08c10b616585bce56b3067cb38b
Ionut Opria <opriaionut14@gmail.com>
2020-11-08 18:00:56 +0200

    IMovable
    Implemented an interface that allows entities to move based on the navigation system.

--- SerenityGardenTD/Assets/_SerenityGardenTD/Scripts/BattleScene/Enemies/EnemyBase.cs
+++ SerenityGardenTD/Assets/_SerenityGardenTD/Scripts/BattleScene/Enemies/EnemyBase.cs
@@ -12,49 +12,95 @@ namespace SerenityGarden
         Ambusher
     }
 
-    public class EnemyBase : LogicProcessBase
+    public class EnemyBase : LogicProcessBase, IMovable
     {
         public EnemyType enemyType;
-        public HexagonalBlock start;
-        public HexagonalBlock end;
-
-        public float distanceThreshold = 1.0f;
-        public float speed = 5.0f;
-        public HexagonalBlock currentBlock;
-        public HexagonalBlock nextBlock;
-
-        public int gridId = 0;
         private NavigationManager navigationManager;
         private HexagonalGrid hexagonalGrid;
 
-        private void Awake()
-        {
-            BaseAwakeCalls();
-        }
+        private HexagonalBlock currentBlock;
+        private HexagonalBlock endBlock;
+        private HexagonalBlock nextBlock;
 
-        private void Start()
+        private float speed = 1.0f;
+        private float reachedThreshold = 0.5f;
+        private bool reachedDestination = false;
+
+        #region Properties
+        public HexagonalBlock CurrentBlock
+        {
+            get { return currentBlock; }
+            set { currentBlock = value; }
+        }
+        public HexagonalBlock EndBlock 
+        { 
+            get { return endBlock; }
+            set { endBlock = value; }
+        }
+        public HexagonalBlock NextBlock 
+        {
+            get { return nextBlock; }
+            set { nextBlock = value; }
+        }
+        public float Speed 
+        { 
+            get { return speed; }
+            set { speed = value; }
+        }
+        public float ReachedThreshold 
+        { 
+            get { return reachedThreshold; }
+            set { reachedThreshold = value; }
+        }
+        public bool ReachedDestination 
+        { 
+            get { return reachedDestination; }
+            set { reachedDestination = value; }
+        }
+        #endregion
+
+        public override void BaseAwakeCalls()
         {
             hexagonalGrid = FindObjectOfType<HexagonalGrid>();
             navigationManager = FindObjectOfType<NavigationManager>();
-            BaseStartCalls();
+            base.BaseAwakeCalls();
+        }
+        public override void BaseUpdateCalls()
+        {
+            base.BaseUpdateCalls();
+            if (nextBlock == null)
+                FindNextBlock();
+            else
+            {
+                ReachedDestination = CheckReached();
+            }
+
+            if (!ReachedDestination)
+                Move();
         }
 
         public override void Init()
         {
-            start = hexagonalGrid.gridCells[gridId];
-            currentBlock = start;
-            transform.position = start.transform.position + Vector3.up * 0.5f;
             float minDist = float.MaxValue;
             for(int index = 0; index < HexagonalGrid.enemyGoal.Count; index++)
             {
                 float dist = HelperMethods.SquaredDistance(transform.position, HexagonalGrid.enemyGoal[index].transform.position);
                 if (dist < minDist)
                 {
-                    end = HexagonalGrid.enemyGoal[index];
+                    endBlock = HexagonalGrid.enemyGoal[index];
                     minDist = dist;
                 }
             }
-            nextBlock = NavigationManager.instance.FindNext(currentBlock, end);
+            if (CurrentBlock != null)
+                nextBlock = NavigationManager.instance.FindNext(currentBlock, EndBlock);
+        }
+
+        //Needs to be called by the script that
+        public void SetStartBlock(HexagonalBlock startBlock)
+        {
+            CurrentBlock = startBlock;
+            if(EndBlock != null)
+                nextBlock = NavigationManager.instance.FindNext(currentBlock, EndBlock);
         }
 
         public override bool HasAllDependencies()
@@ -64,27 +110,32 @@ namespace SerenityGarden
             return false;
         }
 
-        private void Update()
-        {
-            if (nextBlock == null)
-                Debug.LogWarning("Warning! Can't find navigation target");
-            else if(HelperMethods.SquaredDistance(transform.position, nextBlock.transform.position) < distanceThreshold)
-            {
-                FindNextBlock();
-            }
-            if (nextBlock != null)
-                MoveTowards();
-        }
-
-        public void MoveTowards()
+        public void Move()
         {
             transform.position = Vector3.MoveTowards(transform.position, nextBlock.transform.position, speed * Time.deltaTime);
         }
 
+        public bool CheckReached()
+        {
+            if (NextBlock == null)
+            {
+                Debug.LogWarning("Warning! Can't find navigation target");
+                return false;
+            }
+            else if (HelperMethods.SquaredDistance(transform.position, NextBlock.transform.position) < ReachedThreshold)
+            {
+                FindNextBlock();
+                if (NextBlock == null || NextBlock == CurrentBlock)
+                    return true;
+            }
+            return false;
+        }
+
         public void FindNextBlock()
         {
-            currentBlock = nextBlock;
-            nextBlock = NavigationManager.instance.FindNext(currentBlock, end);
+            if(NextBlock != null)
+                CurrentBlock = NextBlock;
+            NextBlock = NavigationManager.instance.FindNext(CurrentBlock, EndBlock);
         }
     }
 }
---
+++ SerenityGardenTD/Assets/_SerenityGardenTD/Scripts/BattleScene/Interfaces/IMovable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SerenityGarden
+{
+    public interface IMovable
+    {
+        HexagonalBlock CurrentBlock { get; set; }
+        HexagonalBlock EndBlock { get; set; }
+        HexagonalBlock NextBlock { get; set; }
+        float Speed { get; set; }
+        float ReachedThreshold { get; set; }
+        bool ReachedDestination { get; set; }
+
+        void FindNextBlock();
+        void Move();
+        bool CheckReached();
+    }
+}
