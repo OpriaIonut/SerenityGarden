11d02718a993ee180da16af6ff2d5b4d0ee6eb87
Ionut Opria <opriaionut14@gmail.com>
2021-06-28 19:15:25 +0300

    Fisiere powerpoint

---
+++ SerenityGardenTD/Assets/_SerenityGardenTD/Scripts/PropellerRotator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerRotator : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.Rotate(Vector3.forward * 500 * Time.deltaTime, Space.Self);
+    }
+}
